3636f0dfe3d30ed7a28bcfcbfe67ac995983a85b
Antti Soininen <antti.soininen@vtt.fi>
2024-11-18 15:53:35 +0200

    Fix parsing of Elering electricity price data responses

--- src/event_loop.rs
+++ src/event_loop.rs
@@ -14,7 +14,7 @@ use chrono::round::DurationRound;
 use chrono::{DateTime, FixedOffset, LocalResult, NaiveDateTime, TimeDelta, TimeZone, Utc};
 use serde::{Deserialize, Serialize};
 use serde_json::Value;
-use std::collections::{BTreeMap, HashMap};
+use std::collections::BTreeMap;
 use std::error::Error;
 use std::io;
 use std::iter;
@@ -812,13 +812,8 @@ fn parse_weather_fetcher_output(
     output: &str,
     time_zone: &impl TimeZone,
 ) -> Result<Vec<(DateTime<FixedOffset>, f64)>, String> {
-    let parsed_json = match serde_json::from_str(output) {
-        Ok(parsed) => parsed,
-        Err(..) => {
-            println!("{}", output);
-            return Err("failed to parse output".to_string());
-        }
-    };
+    let parsed_json = serde_json::from_str(output)
+        .or_else(|error| Err(format!("failed to parse output: {}", error)))?;
     if let Value::Array(time_series) = parsed_json {
         let mut forecast = Vec::with_capacity(time_series.len());
         for row in time_series {
@@ -896,14 +891,12 @@ async fn fetch_electricity_prices(
     start_time: String,
     end_time: String,
     country: &String,
-) -> Result<BTreeMap<DateTime<FixedOffset>, f64>, Box<dyn std::error::Error + Send + Sync>> {
+) -> Result<Vec<(DateTime<FixedOffset>, f64)>, Box<dyn std::error::Error + Send + Sync>> {
     let client = reqwest::Client::new();
     let url = format!(
         "https://dashboard.elering.ee/api/nps/price?start={}&end={}",
         start_time, end_time
     );
-
-    // Fetch the response
     let response = client
         .get(&url)
         .header("accept", "application/json")
@@ -914,35 +907,97 @@ async fn fetch_electricity_prices(
         .text()
         .await
         .map_err(|e| Box::new(e) as Box<dyn std::error::Error + Send + Sync>)?;
+    Ok(parse_elering_response(
+        &response_text,
+        &as_elering_country(country)?,
+    )?)
+}
 
-    // Deserialize the response text into a HashMap
-    let api_response: HashMap<String, Vec<HashMap<String, f64>>> =
-        serde_json::from_str(&response_text)
-            .map_err(|e| Box::new(e) as Box<dyn std::error::Error + Send + Sync>)?;
-
-    // Extract only the prices for the specified country
-    let country_data = api_response.get(country).ok_or_else(|| {
-        Box::new(std::io::Error::new(
-            std::io::ErrorKind::NotFound,
-            "country data not found",
-        )) as Box<dyn std::error::Error + Send + Sync>
-    })?;
-
-    // Convert the prices to the desired format and collect them in BTreeMap
-    let mut series = BTreeMap::<DateTime<FixedOffset>, f64>::new();
-    for entry in country_data {
-        if let (Some(&timestamp), Some(&price)) = (entry.get("timestamp"), entry.get("price")) {
-            if let Some(datetime) = DateTime::<Utc>::from_timestamp(timestamp as i64, 0) {
-                series.insert(datetime.into(), price * 1.0);
-            } else {
-                return Err(Box::new(std::io::Error::new(
-                    std::io::ErrorKind::InvalidData,
-                    "Invalid timestamp",
-                )));
-            }
-        }
+fn parse_elering_response(
+    response_text: &str,
+    elering_country: &str,
+) -> Result<Vec<(DateTime<FixedOffset>, f64)>, String> {
+    let response = serde_json::from_str(response_text)
+        .or_else(|error| Err(format!("failed to parse response: {}", error)))?;
+    let response_object = match response {
+        Value::Object(object) => object,
+        _ => return Err("unexpected response content".to_string()),
+    };
+    let success = match response_object
+        .get("success")
+        .ok_or("'success' field missing in response".to_string())?
+    {
+        Value::Bool(success) => success,
+        _ => return Err("unexpected type for 'success' in response".to_string()),
+    };
+    if !success {
+        return Err("electricity price query unsuccessful".to_string());
     }
-    Ok(series)
+    let data = match response_object
+        .get("data")
+        .ok_or("'data' field missing in response".to_string())?
+    {
+        Value::Object(data) => data,
+        _ => return Err("unexpected type for 'data' in response".to_string()),
+    };
+    let price_data = match data.get(elering_country).ok_or(format!(
+        "requested country '{}' not found in response",
+        elering_country
+    ))? {
+        Value::Array(price_data) => price_data,
+        _ => return Err("unexpected type for country data in response".to_string()),
+    };
+    let mut pairs = Vec::with_capacity(price_data.len());
+    for element in price_data {
+        pairs.push(elering_time_stamp_pair(element)?);
+    }
+    Ok(pairs)
+}
+
+fn as_elering_country(country: &str) -> Result<&str, String> {
+    if country == "Estonia" {
+        return Ok("ee");
+    }
+    if country == "Finland" {
+        return Ok("fi");
+    }
+    if country == "Lithuania" {
+        return Ok("lt");
+    }
+    if country == "Latvia" {
+        return Ok("lv");
+    }
+    Err("unknown or unsupported country".to_string())
+}
+
+fn elering_time_stamp_pair(
+    time_stamp_entry: &Value,
+) -> Result<(DateTime<FixedOffset>, f64), String> {
+    let stamp_object = match time_stamp_entry {
+        Value::Object(stamp_object) => stamp_object,
+        _ => return Err("unexpected type for price entry in response".to_string()),
+    };
+    let time_stamp = match stamp_object
+        .get("timestamp")
+        .ok_or("'timestamp' field missing in price entry")?
+    {
+        Value::Number(t) => t
+            .as_i64()
+            .ok_or("failed to parse time stamp as integer".to_string())?,
+        _ => return Err("unexpected type for time stamp".to_string()),
+    };
+    let time_stamp = DateTime::from_timestamp(time_stamp, 0)
+        .ok_or("failed to convert time stamp to date time".to_string())?;
+    let price = match stamp_object
+        .get("price")
+        .ok_or("'price' field missing in price entry")?
+    {
+        Value::Number(x) => x
+            .as_f64()
+            .ok_or("failed to parse price as float".to_string())?,
+        _ => return Err("unexpected type for price".to_string()),
+    };
+    Ok((time_stamp.fixed_offset(), price))
 }
 
 fn create_and_update_elec_price_data(
@@ -995,30 +1050,28 @@ async fn fetch_electricity_price_task(
     tx: oneshot::Sender<OptimizationData>,
 ) -> Result<(), String> {
     if let Ok(mut optimization_data) = rx.await {
-        if optimization_data.fetch_elec_data {
-            let format_string = "%Y-%m-%dT%H:%M:%S%.3fZ";
-            let start_time = time_line.first().ok_or("time line is empty")?;
-            let start_time_str = start_time.format(&format_string).to_string();
-            let end_time = time_line.last().ok_or("time line is empty")?;
-            let end_time_str = end_time.format(&format_string).to_string();
-            match fetch_electricity_prices(start_time_str, end_time_str, &location.country).await {
-                Ok(prices) => {
-                    create_and_update_elec_price_data(&mut optimization_data, &prices)?;
-                    if tx.send(optimization_data).is_err() {
-                        return Err("fetch_electricity_price_task: failed to send updated optimization data in electricity prices".to_string());
-                    }
+        let format_string = "%Y-%m-%dT%H:%M:%S%.3fZ";
+        let start_time = time_line.first().ok_or("time line is empty")?;
+        let start_time_str = start_time.format(&format_string).to_string();
+        let end_time = time_line.last().ok_or("time line is empty")?;
+        let end_time_str = end_time.format(&format_string).to_string();
+        match fetch_electricity_prices(start_time_str, end_time_str, &location.country).await {
+            Ok(prices) => {
+                if let Err(error) = check_stamps_match(&prices, &time_line) {
+                    return Err(error);
                 }
-                Err(e) => {
-                    return Err(format!(
-                        "fetch_electricity_price_task: failed to fetch electricity price data: {}",
-                        e
-                    ));
+                let prices = prices.iter().map(|pair| (pair.0.clone(), pair.1)).collect();
+                create_and_update_elec_price_data(&mut optimization_data, &prices)?;
+                if tx.send(optimization_data).is_err() {
+                    return Err("fetch_electricity_price_task: failed to send updated optimization data in electricity prices".to_string());
                 }
             }
-        } else if tx.send(optimization_data).is_err() {
-            return Err(
-                "fetch_electricity_price_task: failed to forward optimization data".to_string(),
-            );
+            Err(e) => {
+                return Err(format!(
+                    "fetch_electricity_price_task: failed to fetch electricity price data: {}",
+                    e
+                ));
+            }
         }
         return Ok(());
     } else {
@@ -1352,6 +1405,59 @@ mod tests {
                 assert_eq!(pair.0, expected_time_stamps[i]);
                 assert_eq!(pair.1, expected_temperatures[i]);
             }
+        }
+    }
+    mod as_elering_country {
+        use super::*;
+        #[test]
+        fn gives_correct_country_codes() -> Result<(), String> {
+            assert_eq!(as_elering_country("Estonia")?, "ee");
+            assert_eq!(as_elering_country("Finland")?, "fi");
+            assert_eq!(as_elering_country("Lithuania")?, "lt");
+            assert_eq!(as_elering_country("Latvia")?, "lv");
+            Ok(())
+        }
+        #[test]
+        fn fails_with_unknown_country() {
+            if let Ok(..) = as_elering_country("Mordor") {
+                panic!("call should have failed");
+            }
+        }
+    }
+    mod elering_time_stamp_pair {
+        use super::*;
+        #[test]
+        fn parses_time_stamp_entry() {
+            let entry_json = "{\"timestamp\":1731927600,\"price\":85.0600}";
+            let entry = serde_json::from_str(entry_json).expect("entry JSON should be parseable");
+            let pair =
+                elering_time_stamp_pair(&entry).expect("constructing a pair should not fail");
+            let expected_date_time = Utc
+                .with_ymd_and_hms(2024, 11, 18, 11, 00, 00)
+                .unwrap()
+                .fixed_offset();
+            assert_eq!(pair, (expected_date_time, 85.06));
+        }
+    }
+    mod parse_elering_response {
+        use super::*;
+        #[test]
+        fn parses_response_succesfully() {
+            let response_json = r#"
+{
+    "success":true,
+    "data": {
+        "fi":[{"timestamp":1731927600,"price":70.0500}]
+    }
+}"#;
+            let time_series =
+                parse_elering_response(response_json, "fi").expect("parsing should succeed");
+            let expected_date_time = Utc
+                .with_ymd_and_hms(2024, 11, 18, 11, 00, 00)
+                .unwrap()
+                .fixed_offset();
+            let expected_time_series = vec![(expected_date_time, 70.05)];
+            assert_eq!(time_series, expected_time_series);
         }
     }
 }
